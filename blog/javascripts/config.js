1cabe62116c2c041e45922f58b455391d5f59c73
ruanjacky <ruansgroup@gmail.com>
2015-10-31 11:53:39 +0800

    vitever

--- blog/javascripts/config.js
+++ blog/javascripts/config.js
@@ -1,18 +1,18 @@
 var disqus_shortname = 'hooloodemo';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://hooloo.github.io';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'hooloo';						//Your username of GitHub.
-var repos = 'hooloo.github.io';					//Name of repository that contains your blog.
-var sitetitle = 'Hooloo';						//Name of your blog.
+var hostbase = 'http://ruanjacky.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'ruanjacky';						//Your username of GitHub.
+var repos = 'ruanjacky.github.io';					//Name of repository that contains your blog.
+var sitetitle = 'VITEVER';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
 var suffix = '';								//Suffix of post files, '.' should be included.
 var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
 
 //Sample Config:
 //var disqus_shortname = 'sneezry';
-//var hostbase = 'http://sneezry.com/blog';
-//var githubname = 'sneezry';
+//var hostbase = 'http://vitever.com/blog';
+//var githubname = 'ruanjacky';
 //var repos = 'blog';	
-//var sitetitle = 'Sneezry';
+//var sitetitle = 'VITEVER';
 //var rss = '';
 //var suffix = '.md';
 //var branch = 'gh-pages';
